e748a870691f96a853ada5e38d8ef64ac5a77c1f
MinhLD2003 <minhld8403@gmail.com>
2024-05-13 10:03:58 +0700

    Socket Chat: One to One

---
+++ Socket/Socket_Chat/Client1/Client1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Net.Sockets;
+
+public class Client1
+{
+
+    private const int BUFFER_SIZE = 1024;
+    private const int PORT_NUMBER = 9999;
+
+    static ASCIIEncoding encoding = new ASCIIEncoding();
+
+    public static void Main()
+    {
+
+        try
+        {
+            TcpClient client = new TcpClient();
+
+            // 1. connect
+            client.Connect("127.0.0.1", PORT_NUMBER);
+            Stream stream = client.GetStream();
+
+            Console.WriteLine("Connected to Server.");
+            var reader = new StreamReader(stream);
+            var writer = new StreamWriter(stream);
+            writer.AutoFlush = true;
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Message to Server: ");
+                    string messageToServer = Console.ReadLine().Trim();
+                    if (!string.IsNullOrEmpty(messageToServer))
+                    {
+                        writer.WriteLine(messageToServer);
+                    }
+
+                }
+                catch (IOException e)
+                {
+                    Console.WriteLine("Ex" + e);
+                }
+            }
+            stream.Close();
+            client.Close();
+        }
+
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex);
+        }
+
+        Console.Read();
+    }
+}
---
+++ Socket/Socket_Chat/Server1/Server1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+public class Server1
+{
+
+    private const int BUFFER_SIZE = 1024;
+    private const int PORT_NUMBER = 9999;
+
+    static ASCIIEncoding encoding = new ASCIIEncoding();
+
+    public static void Main()
+    {
+        try
+        {
+            IPAddress address = IPAddress.Parse("127.0.0.1");
+
+            TcpListener listener = new TcpListener(address, PORT_NUMBER);
+
+            // 1. listen
+            listener.Start();
+
+            Console.WriteLine("Server started on " + listener.LocalEndpoint);
+            Console.WriteLine("Waiting for a connection...");
+
+            Socket socket = listener.AcceptSocket();
+            Console.WriteLine("Connection received from " + socket.RemoteEndPoint);
+
+            var stream = new NetworkStream(socket);
+            var reader = new StreamReader(stream);
+            var writer = new StreamWriter(stream);
+            while (true)
+            {
+                try
+                {
+                    // 2. receive
+                    string messageFromClient = reader.ReadLine().Trim();
+
+                    if (string.IsNullOrEmpty(messageFromClient) || messageFromClient.ToUpper() == "EXIT")
+                    {
+                        writer.WriteLine("BYE");
+                        break;
+                    }
+                    Console.WriteLine("Client: " + messageFromClient);
+                }
+                catch (IOException e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+            stream.Close();
+            writer.Close();
+            socket.Close();
+            listener.Stop();
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex);
+        }
+        Console.Read();
+    }
+}
